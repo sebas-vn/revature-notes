6e577f22ced8e8ed5472915166fe12ee62dcb5c1
Sebastian <sebasvn2340@gmail.com>
2021-08-23 15:31:50 -0400

    add HelloWorld class

---
+++ FirstJavaProject/src/com/revature/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.revature;
+
+public class HelloWorld {
+
+	/**
+	 * This is Java documentation which is really helpful
+	 * to describe a method of functionality of a code
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// syso
+		System.out.println("Hello World!");
+	}
+
+}
